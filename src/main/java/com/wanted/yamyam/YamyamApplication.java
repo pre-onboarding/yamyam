9290cdd411191335ab9dd56acd672ab3ab484b90
MyoungJinSeo <sih5673@gmail.com>
2023-11-01 01:10:43 +0900

    Start Setting

---
+++ src/main/java/com/wanted/yamyam/YamyamApplication.java
@@ -0,0 +1,13 @@
+package com.wanted.yamyam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YamyamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YamyamApplication.class, args);
+    }
+
+}
