9f7defad48730799336aeba3afa6d73816d262e4
Seongguk Jeong <seongguk.dev@gmail.com>
2023-11-04 15:41:39 +0900

    chore: Redis 사용을 위한 설정 #17
    시군구 데이터 API 고도화를 위한 Redis 설정을 추가합니다.

---
+++ src/main/java/com/wanted/yamyam/global/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.wanted.yamyam.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    LettuceConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory("yamyam-redis", 6379);
+    }
+
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+}
