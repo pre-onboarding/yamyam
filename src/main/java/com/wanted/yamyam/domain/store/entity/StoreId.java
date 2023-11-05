bce4be6bb1547d31c5c1226cb30c5fe6fd7988ef
MyoungJinSeo <sih5673@gmail.com>
2023-11-05 21:41:14 +0900

    feat : 복합키 설정 #6

--- src/main/java/com/wanted/yamyam/domain/store/entity/StoreId.java
+++ src/main/java/com/wanted/yamyam/domain/store/entity/StoreId.java
@@ -2,10 +2,7 @@ package com.wanted.yamyam.domain.store.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.io.Serializable;
 
@@ -13,9 +10,10 @@ import java.io.Serializable;
 @Embeddable
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class StoreId implements Serializable {
-    @Column
+    @Column(name = "store_name")
     private String name;
-    @Column
+    @Column(name = "store_address")
     private String address;
 }
