620294049589b6e5ea52d1c1529b31ef81631adf
Seongguk Jeong <seongguk.dev@gmail.com>
2023-11-05 16:03:39 +0900

    feat: Discord Webhook 전송을 위한 설정 #19
    Webhook url을 속성값으로 추가하고, Webhook 요청 시 사용할 RestTemplate을 Bean으로 추가하였습니다. 또한, webhook 전송 시 사용할 DTO 를 추가하였습니다.

--- src/main/java/com/wanted/yamyam/YamyamApplication.java
+++ src/main/java/com/wanted/yamyam/YamyamApplication.java
@@ -2,8 +2,12 @@ package com.wanted.yamyam;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
+@EnableConfigurationProperties
 public class YamyamApplication {
 
     public static void main(String[] args) {
