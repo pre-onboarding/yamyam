5cbc181f67043566a429b3031d4f091b10b56def
MyoungJinSeo <sih5673@gmail.com>
2023-11-07 12:51:52 +0900

    rename   : dto 패키지 위치 변경

--- src/main/java/com/wanted/yamyam/api/store/service/StoreSchedulerService.java
+++ src/main/java/com/wanted/yamyam/api/store/service/StoreSchedulerService.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.wanted.yamyam.api.store.dto.response.StoreSchedulerResponse;
+import com.wanted.yamyam.api.store.dto.StoreSchedulerResponse;
 import com.wanted.yamyam.domain.store.entity.Store;
 import com.wanted.yamyam.domain.store.repo.StoreRepository;
 import com.wanted.yamyam.global.exception.ErrorCode;
