79a1c0b75cc65856c9ba4c793b135090d6aae3ce
ks12b0000 <ks12b0000@gmail.com>
2023-11-07 11:00:23 +0900

    test: TestCode 주석제거  #30

--- src/test/java/com/wanted/yamyam/api/store/service/StoreServiceTest.java
+++ src/test/java/com/wanted/yamyam/api/store/service/StoreServiceTest.java
@@ -66,33 +66,33 @@ class StoreServiceTest {
         assertThat(response2.getStores()).isNotNull();
     }
   
-//    @Test
-//    @DisplayName("신규 맛집 평가 점수를 반영하여 맛집 평점 업데이트")
-//    void shouldUpdateStoreRating() {
-//
-//        var testScore = 5;
-//        var testContent = "맛있어요!!";
-//        var testUsername = "식신";
-//        var oldRating = 2.5;
-//        var oldRatingTotalCount = 30L;
-//        var testMember = Member.builder().id(1L).username(testUsername).build();
-//        var testStore = Store.builder().id(1L).rating(oldRating).build();
-//        var testReviewRequest = new ReviewRequest(testScore, testContent);
-//        var testReview = Review.builder()
-//                .member(testMember)
-//                .store(testStore)
-//                .score(testReviewRequest.score())
-//                .content(testReviewRequest.content())
-//                .build();
-//
-//        // 새로운 review를 저장한 후에 count를 수행하므로, 기존 count 값에 + 1을 해줍니다.
-//        when(reviewRepository.countByStoreId(testReview.getStore().getId())).thenReturn(oldRatingTotalCount + 1);
-//
-//        var newRating = "2.6";
-//
-//        assertThat(storeService.updateRating(testReview)).isEqualTo(newRating);
-//
-//    }
+    @Test
+    @DisplayName("신규 맛집 평가 점수를 반영하여 맛집 평점 업데이트")
+    void shouldUpdateStoreRating() {
+
+        var testScore = 5;
+        var testContent = "맛있어요!!";
+        var testUsername = "식신";
+        var oldRating = 2.5;
+        var oldRatingTotalCount = 30L;
+        var testMember = Member.builder().id(1L).username(testUsername).build();
+        var testStore = Store.builder().id(1L).rating(oldRating).build();
+        var testReviewRequest = new ReviewRequest(testScore, testContent);
+        var testReview = Review.builder()
+                .member(testMember)
+                .store(testStore)
+                .score(testReviewRequest.score())
+                .content(testReviewRequest.content())
+                .build();
+
+        // 새로운 review를 저장한 후에 count를 수행하므로, 기존 count 값에 + 1을 해줍니다.
+        when(reviewRepository.countByStoreId(testReview.getStore().getId())).thenReturn(oldRatingTotalCount + 1);
+
+        var newRating = "2.6";
+
+        assertThat(storeService.updateRating(testReview)).isEqualTo(newRating);
+
+    }
 
     @DisplayName("맛집 상세 정보 조회")
     @Test
